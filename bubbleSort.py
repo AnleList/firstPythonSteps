47d4c5a460d58505079a96bf61fe86aeac7b9a84
anle <anle@list.ru>
2025-03-04 19:23:02 +0300

    bubble_sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,27 @@
-test commit after clone rep
+import sys
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Использование: python script.py <число1> <число2> ... <числоN>")
+        sys.exit(1)
+
+    try:
+        # Преобразуем аргументы командной строки в список целых чисел
+        numbers = list(map(int, sys.argv[1:]))
+
+        # Сортируем список методом пузырька
+        bubble_sort(numbers)
+
+        # Выводим отсортированный список
+        print("Отсортированный список:", numbers)
+    except ValueError:
+        print("Ошибка: Все аргументы должны быть целыми числами.")
